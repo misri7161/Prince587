99d33e66812d505be7bc5dbe9848d800f43f191c
misri7161 <misri7161@gmail.com>
2025-04-18 12:55:07 +0500

    Update cpl.js

--- Priyansh/commands/cpl.js
+++ Priyansh/commands/cpl.js
@@ -51,7 +51,7 @@ module.exports.run = async function({ api, event, args }) {
 	let pathImg = __dirname + '/cache/einstein.png';
 	var text = args.join(" ");
 	if (!text) return api.sendMessage("Enter the content of the comment on the board", threadID, messageID);
-	let getPorn = (await axios.get(`https://ibb.co/tpMBVJP4.jpg`, { responseType: 'arraybuffer' })).data;
+	let getPorn = (await axios.get(`https://i.ibb.co/N26xcyns/IMG-20250418-124215.jpg`, { responseType: 'arraybuffer' })).data;
 	fs.writeFileSync(pathImg, Buffer.from(getPorn, 'utf-8'));
 	let baseImage = await loadImage(pathImg);
 	let canvas = createCanvas(baseImage.width, baseImage.height);
